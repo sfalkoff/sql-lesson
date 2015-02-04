a61e98cc8f4feb588a1b92304dbded80a8b7f302
Hackbright Student <info@hackbrightacademy.com>
2015-02-03 17:01:45 -0800

    working through expansion exercises

--- hackbright.py
+++ hackbright.py
@@ -11,16 +11,35 @@ def get_student_by_github(github):
 Student: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
 
+def get_project_by_title(title):
+    query = """SELECT title, description, max_grade FROM Projects WHERE title = ?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    print """\
+Title: %s
+Description: %s
+Max Grade: %s"""%(row[0], row[1], row[2])
+
+def add_project(title, description, max_grade):
+    query = """INSERT INTO Projects (title, description, max_grade) VALUES (?,?,?)"""
+    DB.execute(query, (title, description, max_grade))
+    CONN.commit()
+    print """\
+    Successfully added 
+    Project title: %s 
+    Description: %s
+    Max Grade: %s"""% (title, description, max_grade)
+
 def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("hackbright.db")
     DB = CONN.cursor()
 
 def make_new_student(first_name, last_name, github):
-    query = """INSERT into Students values (?,?,?)"""
+    query = """INSERT INTO Students VALUES (?,?,?)"""
     DB.execute(query, (first_name, last_name, github))
     CONN.commit()
-    print "Successfully added student: %s %s"(first_name, last_name)
+    print "Successfully added student: %s %s" % (first_name, last_name)
 
 def main():
     connect_to_db()
@@ -35,6 +54,10 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "title":
+            get_project_by_title(*args)
+        elif command == "new_project":
+            add_project(*args)
 
     CONN.close()
 
